b2675fbb5f0b3459b6da5f0e04b0dd1c4d75f9a8
seanpsullivan <sean@madebyelmcity.ca>
2020-07-16 09:44:33 -0300

    Update crm-test-code.js

--- crm-test-code.js
+++ crm-test-code.js
@@ -2,8 +2,6 @@ window.onload = function(){
   function formSubmit(event) {
     window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
   }
-
   const form = document.getElementById('createForm');
   form.addEventListener('submit', formSubmit);
-  };
 };
