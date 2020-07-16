11d002783e5aac16a49ab6383b8c07f262db2b70
seanpsullivan <sean@madebyelmcity.ca>
2020-07-16 14:48:25 -0300

    Update crm-test-code.js

--- crm-test-code.js
+++ crm-test-code.js
@@ -1,13 +1,11 @@
 window.onload = function(){
   // send postMessage after form is submitted
   function formSubmit(event) {
-    setTimeout(function(){
-      console.log('counting down from 3!')
+      console.log('running formSubmit function')
       window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
-    }, 3000);
   }
-  const form = document.getElementById('createForm');
-  form.addEventListener('beforeunload', formSubmit);
+  // const form = document.getElementById('createForm');
+  window.addEventListener('beforeunload', formSubmit);
 
   // Force postal code to uppercase
   // https://www.the-art-of-web.com/html/input-field-uppercase/
