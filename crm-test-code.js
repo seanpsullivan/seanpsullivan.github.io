419a2a309e354272f19642a291788f10639782fe
seanpsullivan <sean@madebyelmcity.ca>
2020-08-18 14:47:20 -0300

    Update crm-test-code.js

--- crm-test-code.js
+++ crm-test-code.js
@@ -1,25 +1,16 @@
-let myLocation = location.href;
-console.log("URL is" + location.href);
-
-if (window.location.href.indexOf("inquiry") > -1) {
-   console.log("This is a confirmed submission page");
-//   window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
- }
-
 window.onload = function(){
-  console.log('loaded');
-//   Check if the #createForm element exists
+  console.log("window loaded");
+  // Check if the #createForm element exists
   var formPage = document.getElementById("createForm");
+
   if (formPage) {
-  //  var sendMessage = function (event) {
+    console.log ('i see the form!');
+    // Listen for the beforeunload event after the form submits
+    window.addEventListener('beforeunload', function (e) {
       // Send a postMessage to the parent page
-  //    window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
-  //  };
-      // Listen for an event after the form submits
-
-
-    //  window.addEventListener('beforeunload', sendMessage, false); // Desktop
-    //  window.addEventListener('pagehide', sendMessage, false); //iOS
+      console.log('before unload running')
+      window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
+    });
 
     // Force postal code to uppercase
     // https://www.the-art-of-web.com/html/input-field-uppercase/
@@ -32,5 +23,6 @@ window.onload = function(){
     }
     document.getElementById("address1_postalcode").addEventListener("keyup", forceInputUppercase, false);
 
-} // end if
+  } // end if
+
 }; // end onload
