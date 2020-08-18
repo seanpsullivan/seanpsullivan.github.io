9eaa17dcfe6f730ad6198954b04b29fdc00fd898
seanpsullivan <sean@madebyelmcity.ca>
2020-08-18 15:09:47 -0300

    Update crm-unb-postmessage.js

--- crm-unb-postmessage.js
+++ crm-unb-postmessage.js
@@ -1,38 +1,28 @@
 window.onload = function(){
-  console.log('CRM JS is loaded!');
+   console.log('loaded');
+   // Check if the #createForm element exists
+   var formPage = document.getElementById("createForm");
+   if (formPage) {
+     var sendMessage = function (event) {
+       // Send a postMessage to the parent page
+       window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
+       console.log('postMessage sent!');
+     };
+       // Listen for an event after the form submits
+       window.addEventListener('beforeunload', sendMessage, false); // Desktop
+       window.addEventListener('pagehide', sendMessage, false); //iOS
 
-// Check location
-//  let myLocation = location.href;
-//  console.log("URL is" + location.href);
+      // Force postal code to uppercase
+     // https://www.the-art-of-web.com/html/input-field-uppercase/
+     function forceInputUppercase(e)
+     {
+       var start = e.target.selectionStart;
+       var end = e.target.selectionEnd;
+       e.target.value = e.target.value.toUpperCase();
+       e.target.setSelectionRange(start, end);
+     }
+     document.getElementById("address1_postalcode").addEventListener("keyup", forceInputUppercase, false);
 
-//  if (window.location.href.indexOf("inquirySource") > -1) {
-//     console.log("This is a confirmed submission page");
-//     window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
-//  }
+    } // end if
 
-  // Check if the #createForm element exists
-  var formPage = document.getElementById("createForm");
-  if (formPage) {
-    console.log("it's a form page!");
-    var sendMessage = function (event) {
-      window.parent.postMessage('postMessage says hello!', 'https://www.unb.ca');
-      console.log("postMessage sent");
-    };
-    // Listen for an event after the form submits
-    window.addEventListener('beforeunload', sendMessage, false); // Desktop
-    window.addEventListener('pagehide', sendMessage, false); //iOS
-
-    // Force postal code to uppercase
-    // https://www.the-art-of-web.com/html/input-field-uppercase/
-    function forceInputUppercase(e)
-    {
-      var start = e.target.selectionStart;
-      var end = e.target.selectionEnd;
-      e.target.value = e.target.value.toUpperCase();
-      e.target.setSelectionRange(start, end);
-    }
-    document.getElementById("address1_postalcode").addEventListener("keyup", forceInputUppercase, false);
-
-  } // end if
-
-}; // end onload
+  }; // end onload
