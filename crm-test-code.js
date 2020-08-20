2bbe872abbfb5de0b81d7332ce4fe92e58b569ce
seanpsullivan <sean@madebyelmcity.ca>
2020-08-19 21:43:44 -0300

    Update crm-test-code.js

--- crm-test-code.js
+++ crm-test-code.js
@@ -10,14 +10,27 @@ window.onload = function(){
       console.log("postMessage sent!");
     }
 
-
    // Check if the #createForm element exists
    var formPage = document.getElementById("createForm");
    if (formPage) {
      console.log("This is a form page");
 
      // Find and remove "Request Information" header
-//     document.body.innerHTML = document.body.innerHTML.replace( /Request Information/g, "");
+     document.body.innerHTML = document.body.innerHTML.replace( /Request Information/g, "");
+    // Set country to Canada
+    $('#datatel_address1countryid').val('9acdfc65-203d-e811-80d8-02da73f1e652');
+    // Set student type to High School
+    $('#unb_inquirytype').val('122810000');
+
+
+     // CSS changes to form
+     $('body').css('font-family', 'Nunito Sans');
+     $('.container').css('max-width', '768px');
+     $('.elcn-content').css('border-radius', '0').css('box-shadow', 'none');
+     $('.container.elcn-widget').css('width', '100%');
+
+
+
 
     // Force postal code to uppercase
      // https://www.the-art-of-web.com/html/input-field-uppercase/
