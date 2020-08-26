249f798f81b6a0da1417a35bcba817b129fdd214
seanpsullivan <sean@madebyelmcity.ca>
2020-08-25 21:27:15 -0300

    Tests

--- crm-test-code.js
+++ crm-test-code.js
@@ -20,16 +20,18 @@ window.onload = function(){
     // Set country to Canada
     $('#datatel_address1countryid').val('9acdfc65-203d-e811-80d8-02da73f1e652');
     // Set student type to High School
-//    $('#unb_inquirytype').val('122810000');
+    $('#unb_inquirytype').val('122810000');
 
 
      // CSS changes to form
     $('body').css('font-family', 'Nunito Sans');
-  //   $('.container').css('max-width', '768px');
+    $('.container').css('max-width', '768px');
     $('.elcn-content').css('border-radius', '0').css('box-shadow', 'none');
-  //   $('.container.elcn-widget').css('width', '100%');
+    $('.container.elcn-widget').css('width', '100%');
+    $('input').css('border-radius', '3px');
 
-
+    // Add contest link
+  //  $( ".submit-createaccount-button").after( "<br/><div><p><a href=''>See full contest rules</p></div>");
 
 
     // Force postal code to uppercase
