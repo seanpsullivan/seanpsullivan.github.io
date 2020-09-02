3fb7b7e975c5315eeaefc01207566414d8afab38
seanpsullivan <sean@madebyelmcity.ca>
2020-09-02 13:02:17 -0300

    Update crm-test-code.js

--- crm-test-code.js
+++ crm-test-code.js
@@ -15,16 +15,6 @@ window.onload = function(){
     }
 
    // Form pages only
-
-   // Add GTM to head
-   console.log('adding gtm');
-
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-   })(window,document,'script','dataLayer','GTM-T7FNXXD');
-
    // Check if the #createForm element exists
    var formPage = document.getElementById("createForm");
    if (formPage) {
