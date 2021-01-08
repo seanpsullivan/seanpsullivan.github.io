6a8aad3e7fa664511c7d9404121c92ac1c5b9c5c
seanpsullivan <sean@madebyelmcity.ca>
2021-01-08 12:11:53 -0400

    Create alumni-mag-test-styles.js

---
+++ alumni-mag-test-styles.js
@@ -0,0 +1,25 @@
+
+
+/* Center text on largescreen */
+
+.bkgrblank col-lg-12 h1,
+.bkgrblank col-lg-12 h2,
+.bkgrblank col-lg-12 h3,
+ {
+  max-width:768px;
+}
+.bkgrblank col-lg-12 p {
+  margin: 0 auto 25px auto;
+  max-width: 768px;
+}
+
+/* Remove menu */
+nav.subsite-nav {
+  display:none;
+}
+
+/* Reduce header height */
+header {
+  height:50vh;
+  min-height:auto;
+}
